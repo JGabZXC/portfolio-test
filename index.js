8fa2dceff4ed3428b871505a12b0a1feacd15dbb
JGabZXC <114480911+JGabZXC@users.noreply.github.com>
2024-05-10 14:54:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const navbar = document.querySelector('.navmenu');
+const hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('.primary-header');
+
+hamburger.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    header.classList.toggle("active");
+    
+});
+
+const navItem = document.querySelectorAll('.navitem').length;
+
+for(var i = 0; i < navItem; i++) {
+    document.querySelectorAll('.navitem')[i].addEventListener("click", function() {
+        navbar.classList.remove("active");
+        hamburger.classList.remove("active");
+        header.classList.remove("active");
+        if(navbar.classList.contains("active")) {
+            document.body.style.position = "fixed";
+        } else {
+            document.body.style.position = "static";
+        }
+    });
+}
